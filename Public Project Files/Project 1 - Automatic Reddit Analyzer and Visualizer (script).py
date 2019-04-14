1a7e8e28e979c266ef834205501ae4fd9345763e
Mish Akopyan <imihran@hotmail.com>
2019-04-14 10:23:30 -0700

    updated to script to remove spelling errors

--- Public Project Files/Project 1 - Automatic Reddit Analyzer and Visualizer (script).py
+++ Public Project Files/Project 1 - Automatic Reddit Analyzer and Visualizer (script).py
@@ -5,19 +5,19 @@
 import praw #To connect to reddit and read the comments
 from praw.models import MoreComments #To get only top level comments from reddit
 import matplotlib.pyplot as plt #To make a bar chart at the end showing each posts votes
-import pandas as pd #To convert data in to dataframe which can be exported to google sheets
+import pandas as pd #To convert data in to data-frame which can be exported to google sheets
 import pygsheets #To export the data in to google sheets in order to do visualization, Visualization is done on Google Sheets.
 #-----------------------
 
 #connecting the connection to reddit and google
 #--------------------------
 #connecting to google sheets
-gc = pygsheets.authorize(service_file='Your_Info')
+gc = pygsheets.authorize(service_file='Your_Info.json')
 
 #connecting to reddit
 reddit = praw.Reddit(client_id='Your_Info',
                      client_secret='Your_Info',
-                     user_agent='Your_Info,
+                     user_agent='Your_Info',
                      username ="Your_Info",
                      password ='Your_Info'
                      )
@@ -33,16 +33,16 @@ hot_python = subreddit.hot(limit=20)
 #for every hot post
 for s in hot_python:
     #if the post is no word Meta and is not sticky
-    if not s.stickied and "META" not in s.title.split()[0:10]:
+    if not s.stickied and "META" not in s.title.upper().split()[0:10] and "UPDATE" not in s.title.upper().split()[0:10] and "UPDATE:" not in s.title.upper().split()[0:10]: #updated the loop to exclude posts with "Update" in them. since there is no actual vote is going on.
        # print(s.title)
-       # Initialize the counter to start couting all the top level comment votes
+       # Initialize the counter to start counting all the top level comment votes
         counter = {"Title":'',"URL":'',"SELFTEXT":'',"YTA":0,"NTA":0,"ESH":0,"INFO":0, "NAH":0}
         #for each comment in each post
         for c in s.comments:
-            #this thing somehow only gets the top level comments and makes sure the errors go away. So yeh. I have it here
+            #this thing somehow only gets the top level comments and makes sure the errors go away. So yes. I have it here
             if isinstance(c, MoreComments):
                 continue
-            # a will get the body convert everything to upper case and look for respective Wores.
+            # a will get the body convert everything to upper case and look for respective words.
             a= c.body.upper().split()
             if ("NTA" in a or "NTA." in a or "NTA-" in a or "NTA," in a):
                 counter['NTA'] =counter["NTA"] + 1
@@ -63,7 +63,7 @@ for s in hot_python:
         #append the date 
         tempdf.append(counter)
 
-#confet the dictionarry to dataframe
+#convert the dictionary to data-frame
 df = pd.DataFrame(tempdf)
 
 #count the total votes
@@ -73,14 +73,14 @@ df['Total_Votes'] = df['YTA']+df['NTA']+df['NAH']+df['ESH']+df['INFO']
 df['Arshole_Ratio_%'] = ((df['YTA']+df['ESH']) / df['Total_Votes']) *100
 df['Arshole_Ratio_%'] = round(df['Arshole_Ratio_%'],2)
 
-#rearrainge the dataframe 
+#preparing the data-frame 
 df=df[['Title','YTA', 'NTA','NAH','ESH','Total_Votes','Arshole_Ratio_%','INFO','SELFTEXT', 'URL']]
 
 #sort it by total votes
-#df = df.sort_values(by=['Total_Votes'],ascending=False)
+df = df.sort_values(by=['Total_Votes'],ascending=False)
 
-#push this dataframe to Google Sheets.
+#push this data-frame to Google Sheets.
 sh = gc.open('aitareddit1')
 sh[0].set_dataframe(df,(1,1))
 
-#end
+#end!
